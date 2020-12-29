2751293b6522930ac778081ddb271b33fde0a965
gbonnanzio <gbonnanzio@gmail.com>
2020-12-28 20:34:52 -0500

    all values needed from table found

--- selPrac.py
+++ selPrac.py
@@ -1,17 +1,28 @@
 from selenium import webdriver
 
-driver = webdriver.Chrome(executable_path=r'C:/ChromeDriver/chromedriver.exe')
-driver.get('https://www.wsj.com/market-data/stocks/us/indexes')
-#assert "Python" in driver.title
-table = driver.find_element_by_tag_name('table')
-tbody = table.find_element_by_tag_name('tbody')
-td_list = tbody.find_elements_by_tag_name('td')
-td = td_list[3]
-print(td.get_attribute("innerHTML"))
-#elem = tbody.find_element_by_class_name('WSJTables--table__cell--2u6629rx WSJTheme--table__cell--3njwWeaF ')
-#at = td_list[0].getAttribute()
-#print(at)
+def main():
+    
+    driver = webdriver.Chrome(executable_path=r'C:/ChromeDriver/chromedriver.exe')
+    driver.get('https://www.wsj.com/market-data/stocks/us/indexes')
+    indexes_needed = ['Industrial Average','500 Index','Nasdaq 100','Russell 2000']
+    vals_found = []
+    table = driver.find_element_by_tag_name('table')
+    tbody_list = table.find_elements_by_tag_name('tbody')
+    for tbody in tbody_list:
+        tr_list = tbody.find_elements_by_tag_name('tr')
+        for tr in tr_list:
+            td_list = tr.find_elements_by_tag_name('td')
+            href = td_list[0].find_element_by_tag_name('a')
+            indexName = href.get_attribute("innerHTML")
+            if(indexName in indexes_needed):
+                curr_td_val = td_list[3].get_attribute("innerHTML")
+                vals_found.append(float(curr_td_val))
+                break
+                
+            
+    print(vals_found)
 
-#elem = driver.find_element_by_class_name('WSJTables--table__row--2VdwxeeP')
-driver.close()
-#print(elem)
+
+    driver.close()
+
+main()
