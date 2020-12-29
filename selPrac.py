8ad26ac300c012a2581514c65165f190515c6f34
gbonnanzio <gbonnanzio@gmail.com>
2020-12-28 19:59:24 -0500

    first value pulled from table

--- selPrac.py
+++ selPrac.py
@@ -6,9 +6,11 @@ driver.get('https://www.wsj.com/market-data/stocks/us/indexes')
 table = driver.find_element_by_tag_name('table')
 tbody = table.find_element_by_tag_name('tbody')
 td_list = tbody.find_elements_by_tag_name('td')
+td = td_list[3]
+print(td.get_attribute("innerHTML"))
 #elem = tbody.find_element_by_class_name('WSJTables--table__cell--2u6629rx WSJTheme--table__cell--3njwWeaF ')
-at = td_list[0].getAttribute()
-print(at)
+#at = td_list[0].getAttribute()
+#print(at)
 
 #elem = driver.find_element_by_class_name('WSJTables--table__row--2VdwxeeP')
 driver.close()
